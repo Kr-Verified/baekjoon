95938b553c674a2622c4e161a30023c1aa818d0e
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-05-12 23:31:01 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+n = int(input())
+e = int(input())
+graph = {i: [] for i in range(1, n+1)}
+visited = [False]*(n+1)
+
+for _ in range(e):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+for i in range(1, n+1):
+  graph[i].sort()
+
+
+cnt = 0
+queue = deque([1])
+visited[1] = True
+while queue:
+  node = queue.popleft()
+  for vertex in graph[node]:
+    if not visited[vertex]:
+      queue.append(vertex)
+      visited[vertex] = True
+      if vertex:
+        cnt += 1
+
+print(cnt)
