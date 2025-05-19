21467f479019d5b633ae2fad39c9f74b8bd37240
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-05-19 10:12:08 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 32776 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,29 +1,29 @@
 import sys
-sys.setrecursionlimit(10000)
 input = sys.stdin.readline
+sys.setrecursionlimit(5000)
+visited = [[False for _ in range(51)] for _ in range(51)]
+field = [[0 for _ in range(51)] for _ in range(51)]
+def dfs(x, y):
+  visited[x][y] = True
+  for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+    if 0<=x+dx<len(field) and 0<=y+dy<len(field[0]) and field[x+dx][y+dy]==1 and not visited[x+dx][y+dy]:
+      dfs(x+dx, y+dy)
 
-def dfs(y, x):
-    visited[y][x] = True
-    for dy, dx in [(-1,0), (1,0), (0,-1), (0,1)]:
-        ny, nx = y + dy, x + dx
-        if 0 <= ny < n and 0 <= nx < m:
-            if field[ny][nx] == 1 and not visited[ny][nx]:
-                dfs(ny, nx)
-
-t = int(input())
-for _ in range(t):
-    m, n, k = map(int, input().split())
-    field = [[0 for _ in range(m)] for _ in range(n)]
-    visited = [[False for _ in range(m)] for _ in range(n)]
-    cnt = 0
-    
-    for _ in range(k):
-        x, y = map(int, input().split())
-        field[y][x] = 1
-
-    for y in range(n):
-        for x in range(m):
-            if field[y][x] == 1 and not visited[y][x]:
-                dfs(y, x)
-                cnt += 1
-    print(cnt)
+for _ in range(int(input())):
+  m, n, k = map(int, input().split())
+  field = [[0 for _ in range(m+1)] for _ in range(n+1)]
+  visited = [[0 for _ in range(m+1)] for _ in range(n+1)]
+  cnt = 0
+  # 입력
+  for _ in range(k):
+    y, x = map(int, input().split())
+    field[x][y] = 1
+  
+  #dfs
+  for x in range(n):
+    for y in range(m):
+      if field[x][y]==1 and not visited[x][y]:
+        cnt+=1
+        dfs(x, y)
+  
+  print(cnt)
