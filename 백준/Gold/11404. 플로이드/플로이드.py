1970fc141cb1b54b0bdbfc50429122fb3f415bfe
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-06-25 22:13:33 +0900

    [Gold IV] Title: 플로이드, Time: 556 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,6 +1,13 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 m = int(input())
-cost = [[1000000000 for _ in range(n+1)] for _ in range(n+1)]
+cost = [[float('inf') for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(n+1):
+  cost[i][i] = 0
+
 for _ in range(m):
   a, b, c = map(int, input().split())
   cost[a][b] = min(cost[a][b], c)
@@ -8,10 +15,9 @@ for _ in range(m):
 for k in range(1, n+1):
   for i in range(1, n+1):
     for j in range(1, n+1):
-      if i==j: cost[i][j]=0
       cost[i][j] = min(cost[i][j], cost[i][k]+cost[k][j])
 
 for row in cost[1:]:
   for i in row[1:]:
-    print(0 if i==1000000000 else i, end=' ')
-  print()
+    sys.stdout.write('0 ' if i==float('inf') else str(i) + ' ')
+  sys.stdout.write('\n')
