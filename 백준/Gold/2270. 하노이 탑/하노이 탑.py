0251d37b2c26a842e90d83f98b84090c9e040396
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-07-05 15:32:42 +0900

    [Gold I] Title: 하노이 탑, Time: 184 ms, Memory: 60752 KB -BaekjoonHub

---
+++ 백준/Gold/2270. 하노이 탑/하노이 탑.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def hanoi(n, col):
+  global result
+  if n==0: return
+  for i in range(3):
+    if lis[i] and lis[i][0]==n:
+      if i==col:
+        lis[i].popleft()
+        hanoi(n-1, col)
+      else:
+        temp = 3-i-col
+        lis[i].popleft()
+        hanoi(n-1, temp)
+        result=(result+powers[n-1])%1000000
+        return
+
+result = 0
+lis = []
+n = int(input())
+cols = list(map(int, input().split()))
+powers = [1]*(n+1)
+for i in range(1, n+1):
+  powers[i] = (powers[i-1]*2)%1000000
+for i in range(3):
+    lis.append(deque(map(int, input().split())))
+for i in range(3):
+  if lis[i] and lis[i][0]==n:
+    lis[i].popleft()
+    hanoi(n-1, i)
+    print(i+1)
+    print(result)
+    break
