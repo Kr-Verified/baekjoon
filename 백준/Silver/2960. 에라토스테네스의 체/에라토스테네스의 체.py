316ef092f13d3eeb17cb528769760dbe637204c8
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-07-10 23:04:41 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -1,13 +1,12 @@
 n, k = map(int, input().split())
 prime = [True]*(n+1)
-cnt = 0
 
 for i in range(2, n+1):
   if prime[i]:
     for j in range(i, n+1, i):
       if prime[j]:
         prime[j] = False
-        cnt += 1
-        if cnt==k:
+        k-=1
+        if k==0:
           print(j)
           exit()
