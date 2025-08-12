ef2b333c937e647b0654fa50725343b843cb0cf4
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-08-12 17:51:19 +0900

    [Platinum I] Title: 큰 수 소인수분해, Time: 680 ms, Memory: 32544 KB, Score: 161 point -BaekjoonHub

--- 백준/Platinum/4149. 큰 수 소인수분해/큰 수 소인수분해.py
+++ 백준/Platinum/4149. 큰 수 소인수분해/큰 수 소인수분해.py
@@ -6,10 +6,14 @@ def gcd(a, b):
     a, b = b, a % b
   return a
 
+def is_prime(n):
+  if n <= 71:
+    return n in (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
+  else:
+    return miller_rabin(n)
+
 def miller_rabin(n):
-  if n in (2, 3):
-    return True
-  if n < 2 or n % 2 == 0:
+  if n % 2 == 0:
     return False
   
   s = 0
@@ -18,7 +22,7 @@ def miller_rabin(n):
     d //= 2
     s += 1
 
-  for a in [2, 3, 5, 7, 11, 13, 17]:
+  for a in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]:
     if a >= n:
       break
     x = pow(a, d, n)
@@ -36,32 +40,32 @@ def pollard_rho(n, x=2):
   g = lambda x: (x * x + 1) % n
   temp, y, d = x, x, 1
   
-  for i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]:
-    if n % i == 0:
-      return i
+  if is_prime(n):
+    return n
+  else:
+    for i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]:
+      if n % i == 0:
+        return i
 
-  while True:
-    while d == 1:
-      x = g(x)
-      y = g(g(y))
-      d = gcd(abs(x - y), n)
-    if d == n:
-      temp += 1
-      x = temp
-      y = temp
-      d = 1
-    else:
-      if miller_rabin(d):
-        return d
+    while True:
+      while d == 1:
+        x = g(x)
+        y = g(g(y))
+        d = gcd(abs(x - y), n)
+      if d == n:
+        temp += 1
+        x = temp
+        y = temp
+        d = 1
       else:
-        return pollard_rho(d, 2)
+        if miller_rabin(d):
+          return d
+        else:
+          return pollard_rho(d, 2)
     
 n = int(input())
 answer = []
 while n != 1:
-  if miller_rabin(n):
-    answer.append(n)
-    break
   prime = pollard_rho(n, 2)
   answer.append(prime)
   n //= prime
