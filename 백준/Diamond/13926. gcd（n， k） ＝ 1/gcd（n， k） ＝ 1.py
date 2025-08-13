1d67bf05b39fbee1ce3f0842deb3bfb88f73f31f
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-08-13 18:48:17 +0900

    [Diamond V] Title: gcd(n, k) = 1, Time: 160 ms, Memory: 39776 KB -BaekjoonHub

---
+++ 백준/Diamond/13926. gcd（n， k） ＝ 1/gcd（n， k） ＝ 1.py
@@ -0,0 +1,80 @@
+import sys, random
+sys.setrecursionlimit(10**7)
+
+def gcd(a, b):
+  if a < b: a, b = b, a
+  while b != 0:
+    a, b = b, a % b
+  return a
+
+def miller_rabin(n):
+  if n % 2 == 0:
+    return False
+  
+  s = 0
+  d = n - 1
+  while d % 2 == 0:
+    d //= 2
+    s += 1
+  
+  for a in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]:
+    if a >= n:
+      break
+    x = pow(a, d, n)
+    if x == 1 or x == n - 1:
+      continue
+    for _ in range(s - 1):
+      x = pow(x, 2, n)
+      if x == n - 1:
+        break
+    else:
+      return False
+  return True
+
+def is_prime(n):
+  if n <= 71:
+    return n in (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71)
+  else:
+    return miller_rabin(n)
+
+def pollard_rho(n, x=2):
+  c = 1
+  g = lambda x: (x * x + c) % n
+  temp, y, d = x, x, 1
+
+  if is_prime(n):
+    return n
+  for i in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]:
+    if n % i == 0:
+      return i
+  
+
+  while True:
+    while d == 1:
+      x = g(x)
+      y = g(g(y))
+      d = gcd(abs(x - y), n)
+    if d == n:
+      temp += 1
+      x = temp
+      y = temp
+      d = 1
+    else:
+      if miller_rabin(d):
+        return d
+      else:
+        return pollard_rho(d, 2)
+
+n = int(input())
+
+k = n
+answer = set()
+while k != 1:
+  a = pollard_rho(k)
+  answer.add(a)
+  k //= a
+
+for i in answer:
+  n = n // i * (i - 1)
+
+print(int(n))
