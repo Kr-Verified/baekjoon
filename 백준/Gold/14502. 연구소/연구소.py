172f46aae9d14b9304e538bfb7ac484fbea36bc1
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-09-02 08:48:05 +0900

    [Gold IV] Title: 연구소, Time: 1408 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,7 +1,8 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def wall(cnt, x, y):
+def wall(cnt, idx):
   global visited, r, c, max_cnt
   if cnt == 3:
     visited = [[False for _ in range(c)] for _ in range(r)]
@@ -13,19 +14,20 @@ def wall(cnt, x, y):
       count += row.count(0)
     max_cnt = max(max_cnt, count)
   else:
-    for i in range(r):
-      for j in range(c):
+    for jdx in range(idx, r*c):
+        i, j = divmod(jdx, c)
         if field[i][j] == 0:
           field[i][j] = 1
-          wall(cnt + 1, i, j)
+          wall(cnt + 1, jdx + 1)
           field[i][j] = 0
 
 def bfs(temp, start_x, start_y):
-  queue = [(start_x, start_y)]
+  queue = deque()
+  queue.append((start_x, start_y))
   visited[start_x][start_y] = True
   
   while queue:
-    node_x, node_y = queue.pop(0)
+    node_x, node_y = queue.popleft()
     for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
       nx, ny = node_x + x, node_y + y
       if 0 <= nx < r and 0 <= ny < c and not visited[nx][ny] and temp[nx][ny] == 0:
@@ -38,5 +40,5 @@ field = [list(map(int, input().split())) for _ in range(r)]
 visited = [[False for _ in range(c)] for _ in range(r)]
 positions = [(i, j) for i in range(r) for j in range(c) if field[i][j] == 2]
 max_cnt = 0
-wall(0, 0, 0)
+wall(0, 0)
 print(max_cnt)
