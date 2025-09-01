2c87a24527359b5f6f77c0312d6456cac7622117
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-09-02 08:40:56 +0900

    [Gold IV] Title: 연구소, Time: 1864 ms, Memory: 114548 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+def wall(cnt, x, y):
+  global visited, r, c, max_cnt
+  if cnt == 3:
+    visited = [[False for _ in range(c)] for _ in range(r)]
+    temp = [row[:] for row in field]
+    count = 0
+    for dx, dy in positions:
+      bfs(temp, dx, dy)
+    for row in temp:
+      count += row.count(0)
+    max_cnt = max(max_cnt, count)
+  else:
+    for i in range(r):
+      for j in range(c):
+        if field[i][j] == 0:
+          field[i][j] = 1
+          wall(cnt + 1, i, j)
+          field[i][j] = 0
+
+def bfs(temp, start_x, start_y):
+  queue = [(start_x, start_y)]
+  visited[start_x][start_y] = True
+  
+  while queue:
+    node_x, node_y = queue.pop(0)
+    for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+      nx, ny = node_x + x, node_y + y
+      if 0 <= nx < r and 0 <= ny < c and not visited[nx][ny] and temp[nx][ny] == 0:
+        visited[nx][ny] = True
+        temp[nx][ny] = 2
+        queue.append((nx, ny))
+
+r, c = map(int, input().split())
+field = [list(map(int, input().split())) for _ in range(r)]
+visited = [[False for _ in range(c)] for _ in range(r)]
+positions = [(i, j) for i in range(r) for j in range(c) if field[i][j] == 2]
+max_cnt = 0
+wall(0, 0, 0)
+print(max_cnt)
