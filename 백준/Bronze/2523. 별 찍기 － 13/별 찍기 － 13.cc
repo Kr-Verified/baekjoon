26e42df31e0ed7e832ceb4328566e3be2de5ec3e
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-09-21 20:31:02 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+
+  for (int i = n - 1; i > 0; i--) {
+    for (int j = 0; j < i; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
+
+  return 0;
+}
