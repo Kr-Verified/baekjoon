c745c0131fa011aebf9327362428490eb25de14a
Kr-Verified <131538446+Kr-Verified@users.noreply.github.com>
2025-09-28 20:29:44 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1516 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int n, idx;
+  cin >> n;
+
+  vector<int> v(10001, 0);
+  for (int i = 0; i < n; i++) {
+    cin >> idx;
+    v[idx]++;
+  }
+
+  for (int i = 1; i <= 10000; i++) {
+    while (v[i]--) cout << i << '\n';
+  }
+
+  return 0;
+}
